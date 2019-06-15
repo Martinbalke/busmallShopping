11c21633ebf4182afb8b67574e3d0e17f1614134
Martin Balke <Mrtnbalke@gmail.com>
2019-06-15 14:46:52 -0700

    finished additem and savetolocal

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 'use strict';
+var formEL = document.getElementById('catalog');
+var optionsEL = document.getElementById('items');
+var quantityEL = document.getElementById('quantity');
+var items = [];
 
 // Cart constructor.
 var Cart = function(items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
+var myCart = new Cart(items);
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  product = optionsEL.value;
+  quantity = quantityEL.value;
+  items.push(new CartItem(product, quantity));
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
 };
+
+function addCartItem(e){
+  e.preventDefault();
+  myCart.addItem();
+  myCart.saveToLocalStorage();
+}
 
 var CartItem = function(product, quantity) {
   this.product = product;
@@ -54,6 +68,6 @@ function generateCatalog() {
   new Product('assets/water-can.jpg', 'Water Can');
   new Product('assets/wine-glass.jpg', 'Wine Glass');
 }
-
+formEL.addEventListener('submit', addCartItem)
 // Initialize the app by creating the big list of products with images and names
 generateCatalog();
